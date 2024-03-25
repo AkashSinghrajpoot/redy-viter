edcbb887defdf0b9b04b9e3fcf649d05828da37d
AkashSinghrajpoot <164709670+AkashSinghrajpoot@users.noreply.github.com>
2024-03-25 14:23:41 +0530

    first commit

---
+++ deekisi/src/components/Experience.jsx
@@ -0,0 +1,20 @@
+import { OrbitControls } from "@react-three/drei";
+
+import { BoxGeometry } from "three";
+
+export const Experience = () => {
+  return (
+    <>
+    <OrbitControls />
+    <mesh>
+    <meshNormalMaterial/>
+    <boxBufferGeometry/>
+
+
+    </mesh>
+      
+      
+    </>
+  );
+};
+ 
